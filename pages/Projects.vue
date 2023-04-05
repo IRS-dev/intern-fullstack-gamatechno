48a661f474d09ea6f307ce57a47ee2a0abbc003d
irsyad aliyahya <irsyadalyahya5@gmail.com>
2023-04-05 10:30:53 +0700

    1st

---
+++ pages/Projects.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="about page animate__animated animate__fadeIn">
+    <h2>Recent Project</h2>
+    <div class="container">
+      <div class="row" v-for="(project, index) in projects" :key="index">
+        <div class="col-md-5">
+          <img
+            :src="project.image_cover"
+            :alt="project.title"
+            class="project-cover"
+          />
+        </div>
+        <div class="col-md-5">
+          <h2 class="mt-3" style="text-align: left">
+            {{ project.title }}
+            <hr class="dope" />
+          </h2>
+          <b-card class="mt-3 mb-3 text-left text-dark">
+            <p>{{ project.description }}</p>
+            <b-link :href="project.github_url"
+            >View source code <GithubIcon style="color: #000"
+          /></b-link>
+          </b-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import LinkIcon from "vue-ionicons/dist/md-link.vue";
+import GithubIcon from "vue-ionicons/dist/logo-github.vue";
+
+export default {
+  components: {
+    GithubIcon,
+    LinkIcon,
+  },
+  data() {
+    return {
+      projects: [
+        {
+          title: "Laravel Blog",
+          tag: "Progressive Web App",
+          description:
+            "Simple blog using laravel. implement CRUD and clean code",
+          image_cover: require("@/assets/speak.jpg"),
+          github_url: "https://github.com/IRS-dev/laravel-Blog-005",
+        },
+        {
+          title: "Laravel Lumen REST API",
+          tag: "Progressive Web App",
+          description:
+            "lumen simple REST API (CRUD)",
+          image_cover: require("@/assets/illustration.jpg"),
+          github_url: "https://github.com/IRS-dev/lumen-rest-api",
+        },
+        {
+          title: "Laravel Scrap Movie Data",
+          tag: "Progressive Web App",
+          description:
+            "Scrapping data from movie website",
+          image_cover: require("@/assets/qr-1.png"),
+          github_url: "https://github.com/IRS-dev/laravel-scrap-movie",
+        },
+        {
+          title: "Vue fetch API",
+          tag: "Progressive Web App",
+          description:
+            "Scrapping data from movie website",
+          image_cover: require("@/assets/qr-1.png"),
+          github_url: "https://github.com/IRS-dev/vue-fetch-api",
+        },
+      ],
+    };
+  },
+  methods: {
+    isEven(n) {
+      if (n % 2 == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  head: {
+    title: "Recent Projects 💻 - Muhammad Irsyad Aliyahya ",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "View all the recents open-sourced projects built with ♥ by Asaolu Elijah, open-sourced means this project source code are publicly available for everyone to use or modify.",
+      },
+      {
+        hid: "og:title",
+        name: "og:title",
+        content: "Recent Projects 💻 - Muhammad Irsyad Aliyahya",
+      },
+      {
+        property: "og:description",
+        content:
+          "View all the recents open-sourced projects built with ♥ by Asaolu Elijah, open-sourced means this project source code are publicly available for everyone to use or modify.",
+      },
+      {
+        hid: "og:image",
+        name: "og:image",
+        content: require("@/assets/man-computer.png"),
+      },
+    ],
+  },
+};
+</script>
+<style scoped>
+.row {
+  margin-top: 70px;
+}
+.project-cover {
+  width: inherit;
+  border-radius: 3px;
+}
+hr.dope {
+  border: 0;
+  width: 90px;
+  border-top: 2px solid #dc3545;
+  text-align: left;
+  margin: initial;
+  margin-top: 7px;
+  margin-left: 5px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,113 @@
+<template>
+  <b-row style="margin-left: 0; margin-right: 0">
+    <b-col md="4">
+      <b-img :src="avatar" id="avatar-img" alt="Muhammad Irsyad Aliyahya" />
+    </b-col>
+    <b-col md="8">
+      <h2 class="intro animate__animated animate__fadeInUp animate__fast">
+        Hi 👋,
+        <br />I<span>'m Muhammad Irsyad Aliyahya</span>.
+      </h2>
+      <div class="col-md-10 info">
+        A <b>student</b> and <b>website development enthusiast</b> passionate
+        about solving problems with tech.<br />
+        I am skilled with building beautiful <b>websites</b> and currently i'm
+        exploring <b>vuejs</b>.
+        <br />
+        <b-button
+          class="action-btn animate__animated animate__wobble animate__delay-4s animate__fast"
+          to="/projects"
+        >
+          Explore >
+        </b-button>
+        <b-button class="action-btn" to="/contact" variant="primary">
+          Get In Touch
+        </b-button>
+        <!-- .SOCIAL LINKS SECTION -->
+        <div
+          class="social-icons animate__animated animate__fadeInUp animate__delay-1s"
+        >
+          <social-link :to="socialLinks.github">
+            <GithubIcon/>
+          </social-link>
+
+          <social-link :to="socialLinks.linkedin">
+            <LinkedinIcon/>
+          </social-link>
+
+          <social-link :to="socialLinks.mail">
+            <MailIcon/>
+          </social-link>
+
+          <social-link :to="socialLinks.youtube">
+            <YoutubeIcon/>
+          </social-link>
+        </div>
+      </div>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+// import icon
+import GithubIcon from "vue-ionicons/dist/logo-github.vue";
+import LinkedinIcon from "vue-ionicons/dist/logo-linkedin.vue";
+import YoutubeIcon from "vue-ionicons/dist/logo-youtube.vue";
+import MailIcon from "vue-ionicons/dist/md-mail.vue";
+
+export default {
+  components: {
+    GithubIcon,
+    LinkedinIcon,
+    YoutubeIcon,
+    MailIcon,
+  },
+
+  data() {
+    return {
+      avatar: require("@/assets/person.jpeg"),
+      // Sosial Link
+      socialLinks: {
+        github: "https://github.com/IRS-dev",
+        linkedin:"https://www.linkedin.com/in/muhammad-irsyad-aliyahya-698049222/",
+        mail: "mailto:irsyadalyahya5@gmail.com",
+        youtube: "",
+      },
+    };
+  },
+
+  head: {
+    title: "Portfolio ⚡ - Muhammad Irsyad Aliyahya",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content:
+          " A student and website development enthusiast passionate about solving problems with tech I am skilled with building beautiful websites and currently i'm exploring vuejs.",
+      },
+      {
+        hid: "og:title",
+        name: "og:title",
+        content: "Portfolio ⚡ - Muhammad Irsyad Aliyahya",
+      },
+      {
+        property: "og:description",
+        content:
+        " A student and website development enthusiast passionate about solving problems with tech I am skilled with building beautiful websites and currently i'm exploring vuejs.",
+      },
+      {
+        hid: "og:image",
+        name: "og:image",
+        content: require("@/assets/coding.png"),
+      },
+    ],
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start();
+
+      setTimeout(() => this.$nuxt.$loading.finish(), 500);
+    });
+  },
+};
+</script>
