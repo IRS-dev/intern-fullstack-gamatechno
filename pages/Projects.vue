b1229b94d416c280adf04b4fa83039306c2d2fb1
irsyad alyahya <irsyadalyahya5@gmail.com>
2023-04-07 08:59:05 +0700

    fixing project image

--- pages/Projects.vue
+++ pages/Projects.vue
@@ -36,8 +36,8 @@ export default {
   },
   data() {
     return {
-      // wkwk: require ("@/assets/laravel.jpg"),
-      // sst: require ("@/assets/vue.png"),
+      laravel: require("@/assets/laravel.jpg"),
+      vue: require("@/assets/vue.png"),
       project: null,
     };
   },
